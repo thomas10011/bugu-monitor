65473aaf2fd983d9e4f001f1107bc536847300a2
thomas10011 <tanhuiri@126.com>
2020-08-19 17:00:36 +0800

    添加阿里云OSS服务端签名工具类

---
+++ common-utils/src/main/java/cn/fusionfuture/bugu/utils/oss/OssUtilProperties.java
@@ -0,0 +1,28 @@
+package cn.fusionfuture.bugu.utils.oss;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author thomas
+ * @version 1.0
+ * @class OssConfig
+ * @description 用于动态从yml配置文件中获取bucket和endpoint
+ * @date 2020/8/19 2:34 下午
+ */
+@Data
+@Component
+@ConfigurationProperties(prefix = "spring.cloud.alicloud.oss")
+@PropertySource(value = "classpath:application.yml", encoding = "UTF-8")
+public class OssUtilProperties {
+
+    private String endpoint;
+
+    private String bucket;
+
+    private String callbackUrl;
+
+}
