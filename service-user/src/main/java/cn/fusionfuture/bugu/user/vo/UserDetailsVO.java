7c43a1ba83ec5b6e705231820913e5ca3b124195
Aki <Aki@Aki.com>
2020-09-10 21:25:04 +0800

    更新nacos&redis配置

---
+++ service-user/src/main/java/cn/fusionfuture/bugu/user/vo/UserDetailsVO.java
@@ -0,0 +1,47 @@
+package cn.fusionfuture.bugu.user.vo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.math.BigDecimal;
+
+/**
+ * @author Aki
+ * @version 1.0
+ * @class UserDertailsVO
+ * @description 用户信息
+ * @date 2020/9/8 16:46
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@ApiModel(value="UserDetailsVO对象", description="")
+public class UserDetailsVO {
+
+    @ApiModelProperty(value = "用户名")
+    private String name;
+
+    @ApiModelProperty(value = "用户头像")
+    private String avator;
+
+    @ApiModelProperty(value = "关注数")
+    private Integer followQuantity;
+
+    @ApiModelProperty(value = "粉丝数")
+    private Integer fansQuantity;
+
+    @ApiModelProperty(value = "性别")
+    private Boolean gender;
+
+    @ApiModelProperty(value = "羽毛数")
+    private BigDecimal featherBalance;
+
+    @ApiModelProperty(value = "现金余额")
+    private BigDecimal cashBalance;
+
+    @ApiModelProperty(value = "今日羽毛数变化")
+    private BigDecimal featherChange;
+}
