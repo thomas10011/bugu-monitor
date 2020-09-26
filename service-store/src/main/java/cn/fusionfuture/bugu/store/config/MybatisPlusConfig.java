8bbe891922a2d3b0941dc295755b234797b352f2
thomas10011 <tanhuiri@126.com>
2020-09-26 14:25:40 +0800

    各个微服务添加mybatis plus自动填充

---
+++ service-store/src/main/java/cn/fusionfuture/bugu/store/config/MybatisPlusConfig.java
@@ -0,0 +1,27 @@
+package cn.fusionfuture.bugu.store.config;
+
+import cn.fusionfuture.bugu.dispose.fill.CustomMetaObjectHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author thomas
+ * @version 1.0
+ * @class MybatisPlusConfig
+ * @description 配置mybatis plus
+ * @date 2020/9/26 2:21 下午
+ */
+@Configuration
+public class MybatisPlusConfig {
+    /**
+     * @author thomas
+     * @description 将CustomMetaObjectHandler注入IOC容器 用于对字段进行自动填充
+     * @create 2020/8/17 7:27 下午
+     * @update 2020/8/17 7:27 下午
+     * @return cn.fusionfuture.bugu.dispose.fill.CustomMetaObjectHandler
+     **/
+    @Bean
+    public CustomMetaObjectHandler customMetaObjectHandler() {
+        return new CustomMetaObjectHandler();
+    }
+}
---
+++ service-user/src/main/java/cn/fusionfuture/bugu/user/config/UtilsConfig.java
@@ -0,0 +1,16 @@
+package cn.fusionfuture.bugu.user.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author thomas
+ * @version 1.0
+ * @class UtilsConfig
+ * @description 扫描工具里所在包
+ * @date 2020/8/19 4:29 下午
+ */
+@Configuration
+@ComponentScan("cn.fusionfuture.bugu.utils")
+public class UtilsConfig {
+}
