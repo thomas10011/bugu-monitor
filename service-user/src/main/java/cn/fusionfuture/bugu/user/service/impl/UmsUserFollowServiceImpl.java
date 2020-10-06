23907f3d4d15594d951db6616aafc782568997df
thomas10011 <tanhuiri@126.com>
2020-10-06 12:49:34 +0800

    修复用户关注和取关没有修改对应数据的bug

--- service-user/src/main/java/cn/fusionfuture/bugu/user/service/impl/UmsUserFollowServiceImpl.java
+++ service-user/src/main/java/cn/fusionfuture/bugu/user/service/impl/UmsUserFollowServiceImpl.java
@@ -1,7 +1,9 @@
 package cn.fusionfuture.bugu.user.service.impl;
 
+import cn.fusionfuture.bugu.pojo.entity.UmsUser;
 import cn.fusionfuture.bugu.pojo.entity.UmsUserFollow;
 import cn.fusionfuture.bugu.user.mapper.UmsUserFollowMapper;
+import cn.fusionfuture.bugu.user.mapper.UmsUserMapper;
 import cn.fusionfuture.bugu.user.service.IUmsUserFollowService;
 import cn.fusionfuture.bugu.user.vo.UserFollowVO;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -11,7 +13,6 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
@@ -26,33 +27,43 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class UmsUserFollowServiceImpl extends ServiceImpl<UmsUserFollowMapper, UmsUserFollow> implements IUmsUserFollowService {
 
     @Autowired
-    UmsUserFollowMapper umsUserFollowMapper;
+    UmsUserFollowMapper userFollowMapper;
+
+    @Autowired
+    UmsUserMapper userMapper;
 
     @Override
     public PageInfo<UserFollowVO> queryUmsUserFollowByPage(Integer pn, Integer ps, Long uid) {
         Page<UserFollowVO> page = PageHelper.startPage(pn, ps);
-        return new PageInfo<>(umsUserFollowMapper.queryUmsUserFollowByPage(uid));
+        return new PageInfo<>(userFollowMapper.queryUmsUserFollowByPage(uid));
     }
 
     @Override
     public PageInfo<UserFollowVO> queryUmsUserFansByPage(Integer pn, Integer ps, Long uid) {
         Page<UserFollowVO> page = PageHelper.startPage(pn, ps);
-        return new PageInfo<>(umsUserFollowMapper.queryUmsUserFansByPage(uid));
+        return new PageInfo<>(userFollowMapper.queryUmsUserFansByPage(uid));
     }
 
     @Override
     public Boolean followUser(Long uid, @RequestParam Long fuid) {
-        UmsUserFollow obj = umsUserFollowMapper.selectOne(
+        UmsUserFollow obj = userFollowMapper.selectOne(
                 new QueryWrapper<UmsUserFollow>()
                     .eq("user_id", uid)
                     .eq("followed_user_id", fuid)
         );
         if (obj == null) {
-            umsUserFollowMapper.insert(
+            userFollowMapper.insert(
                     new UmsUserFollow()
                     .setFollowedUserId(fuid)
                     .setUserId(uid)
             );
+            // 被关注的用户粉丝数+1 关注的用户关注数+1
+            UmsUser user = userMapper.selectById(uid);
+            UmsUser followedUser = userMapper.selectById(fuid);
+
+            userMapper.updateById(user.setFollowQuantity(user.getFollowQuantity() + 1));
+            userMapper.updateById(followedUser.setFansQuantity(followedUser.getFansQuantity() + 1));
+
             return true;
         }
         return false;
@@ -60,9 +71,18 @@ public class UmsUserFollowServiceImpl extends ServiceImpl<UmsUserFollowMapper, U
 
     @Override
     public Boolean unFollowUser(Long uid, @RequestParam Long fuid) {
-        umsUserFollowMapper.delete(new QueryWrapper<UmsUserFollow>()
+        userFollowMapper.delete(new QueryWrapper<UmsUserFollow>()
                 .eq("user_id", uid)
                 .eq("followed_user_id", fuid));
+
+        // 被关注的用户粉丝数-1 关注的用户关注数-1
+        UmsUser user = userMapper.selectById(uid);
+        UmsUser followedUser = userMapper.selectById(fuid);
+
+        // TODO: 如果用户关注数和粉丝数小于1 那么应该抛出异常
+
+        userMapper.updateById(user.setFollowQuantity(user.getFollowQuantity() - 1));
+        userMapper.updateById(followedUser.setFansQuantity(followedUser.getFansQuantity() - 1));
 
         return true;
     }
