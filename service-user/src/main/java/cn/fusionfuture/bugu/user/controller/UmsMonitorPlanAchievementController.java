0a8cd5d0bb2f6e8f9a72fa927ce14f3793cb7e5e
thomas10011 <tanhuiri@126.com>
2020-10-19 11:11:40 +0800

    提供更新用户成就相关接口

--- service-user/src/main/java/cn/fusionfuture/bugu/user/controller/UmsMonitorPlanAchievementController.java
+++ service-user/src/main/java/cn/fusionfuture/bugu/user/controller/UmsMonitorPlanAchievementController.java
@@ -1,9 +1,15 @@
 package cn.fusionfuture.bugu.user.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import cn.fusionfuture.bugu.pojo.entity.UmsMonitorPlanAchievement;
+import cn.fusionfuture.bugu.pojo.entity.UmsPkPlanAchievement;
+import cn.fusionfuture.bugu.user.service.IUmsMonitorAchievementService;
+import cn.fusionfuture.bugu.user.service.IUmsMonitorPlanAchievementService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -14,7 +20,42 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2020-08-17
  */
 @RestController
-@RequestMapping("/user/ums-monitor-plan-achievement")
+@RequestMapping("/user")
+@Api(tags = "用户的监督计划相关成就")
 public class UmsMonitorPlanAchievementController {
+
+    @Autowired
+    IUmsMonitorPlanAchievementService achievementService;
+
+    @GetMapping(value = "/achievement/monitor-plan/plan-count/{uid}")
+    @ApiOperation(value = "供计划微服务远程调用 更新用户的监督计划的总数，总数+1")
+    void updatePlanCount(@ApiParam(value = "用户id") @PathVariable(value = "uid") Long uid) {
+        UmsMonitorPlanAchievement achievement = achievementService.getById(uid);
+        achievementService.updateById(
+                new UmsMonitorPlanAchievement()
+                        .setUserId(uid)
+                        .setPlanCount(achievement.getPlanCount() + 1));
+    }
+
+    @GetMapping(value = "/achievement/monitor-plan/success-count/{uid}")
+    @ApiOperation(value = "供计划微服务远程调用 更新用户的监督计划成功次数，总数+1")
+    void updateSuccessCount(@ApiParam(value = "用户id") @PathVariable(value = "uid") Long uid) {
+        UmsMonitorPlanAchievement achievement = achievementService.getById(uid);
+        achievementService.updateById(
+                new UmsMonitorPlanAchievement()
+                        .setUserId(uid)
+                        .setSuccessCount(achievement.getSuccessCount() + 1));
+    }
+
+    @GetMapping(value = "/achievement/monitor-plan/participate-count/{uid}")
+    @ApiOperation(value = "供计划微服务远程调用 更新用户的监督计划的参与人数，总数+1")
+    void updateParticipateCount(@ApiParam(value = "用户id") @PathVariable(value = "uid") Long uid) {
+        UmsMonitorPlanAchievement achievement = achievementService.getById(uid);
+        achievementService.updateById(
+                new UmsMonitorPlanAchievement()
+                        .setUserId(uid)
+                        .setParticipateCount(achievement.getParticipateCount() + 1));
+    }
+
 
 }
--- service-user/src/main/java/cn/fusionfuture/bugu/user/controller/UmsPkPlanAchievementController.java
+++ service-user/src/main/java/cn/fusionfuture/bugu/user/controller/UmsPkPlanAchievementController.java
@@ -1,9 +1,15 @@
 package cn.fusionfuture.bugu.user.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import cn.fusionfuture.bugu.pojo.entity.UmsMonitorAchievement;
+import cn.fusionfuture.bugu.pojo.entity.UmsPkPlanAchievement;
+import cn.fusionfuture.bugu.user.service.IUmsMonitorAchievementService;
+import cn.fusionfuture.bugu.user.service.IUmsPkPlanAchievementService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -14,7 +20,51 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2020-08-17
  */
 @RestController
-@RequestMapping("/user/ums-pk-plan-achievement")
+@RequestMapping("/user")
+@Api(tags = "用户的pk计划相关成就")
 public class UmsPkPlanAchievementController {
+
+    @Autowired
+    IUmsPkPlanAchievementService pkPlanAchievementService;
+
+    @GetMapping(value = "/achievement/pk-plan/plan-count/{uid}")
+    @ApiOperation(value = "供计划微服务远程调用 更新用户的pk计划的总数，总数+1")
+    void updatePlanCount(@ApiParam(value = "用户id") @PathVariable(value = "uid") Long uid) {
+        UmsPkPlanAchievement achievement = pkPlanAchievementService.getById(uid);
+        pkPlanAchievementService.updateById(
+                new UmsPkPlanAchievement()
+                        .setUserId(uid)
+                        .setPlanCount(achievement.getPlanCount() + 1));
+    }
+
+    @GetMapping(value = "/achievement/pk-plan/success-count/{uid}")
+    @ApiOperation(value = "供计划微服务远程调用 更新用户pk计划的成功次数，总数+1")
+    void updateSuccessCount(@ApiParam(value = "用户id") @PathVariable(value = "uid") Long uid) {
+        UmsPkPlanAchievement achievement = pkPlanAchievementService.getById(uid);
+        pkPlanAchievementService.updateById(
+                new UmsPkPlanAchievement()
+                        .setUserId(uid)
+                        .setSuccessCount(achievement.getSuccessCount() + 1));
+    }
+
+    @GetMapping(value = "/achievement/pk-plan/defeat-count/{uid}")
+    @ApiOperation(value = "供计划微服务远程调用 更新用户战胜的人数，总数+1")
+    void updateDefeatCount(@ApiParam(value = "用户id") @PathVariable(value = "uid") Long uid) {
+        UmsPkPlanAchievement achievement = pkPlanAchievementService.getById(uid);
+        pkPlanAchievementService.updateById(
+                new UmsPkPlanAchievement()
+                        .setUserId(uid)
+                        .setDefeatCount(achievement.getDefeatCount() + 1));
+    }
+
+    @GetMapping(value = "/achievement/pk-plan/victory-count/{uid}")
+    @ApiOperation(value = "供计划微服务远程调用 更新用户胜利次数，总数+1")
+    void updateVictoryCount(@ApiParam(value = "用户id") @PathVariable(value = "uid") Long uid) {
+        UmsPkPlanAchievement achievement = pkPlanAchievementService.getById(uid);
+        pkPlanAchievementService.updateById(
+                new UmsPkPlanAchievement()
+                        .setUserId(uid)
+                        .setVictoryCount(achievement.getVictoryCount() + 1));
+    }
 
 }
